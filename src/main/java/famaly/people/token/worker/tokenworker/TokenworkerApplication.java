a51e4678ed4532e4406cac4ba77ea73d0efa155e
crs94787 <ccrs94@mail.ru>
2019-01-22 01:11:07 +0300

    Initial commit

---
+++ src/main/java/famaly/people/token/worker/tokenworker/TokenworkerApplication.java
@@ -0,0 +1,14 @@
+package famaly.people.token.worker.tokenworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenworkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokenworkerApplication.class, args);
+    }
+
+}
+
