6c45e5aafdd1fba68a33fcc900ab9d5757f4e13e
crs94787 <ccrs94@mail.ru>
2019-01-22 01:25:11 +0300

    create configuration pack

--- src/main/java/famaly/people/token/worker/tokenworker/services/TokenWorkerService.java
+++ src/main/java/famaly/people/token/worker/tokenworker/services/TokenWorkerService.java
@@ -1,11 +1,21 @@
 package famaly.people.token.worker.tokenworker.services;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(path = "/auth/")
 public class TokenWorkerService {
 
-    @RequestMapping(path ="isto")
+    @RequestMapping(path ="/session/authorisation", method = RequestMethod.POST)
+    public @ResponseBody
+    ResponseEntity<String token> getAuthorisation(@RequestBody User userAuthData){
+        return null;
+    }
+
 }
